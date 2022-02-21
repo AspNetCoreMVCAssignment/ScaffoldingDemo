7a20c31eccd045c1ff83ad1eea8f95662f5a53ea
Nikunj018 <delvadiyanikunj1234@gmail.com>
2022-02-21 07:52:12 +0530

    Add project files.

---
+++ ScaffoldingDemo/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScaffoldingDemo.Models
+{
+    public class LoginModel
+    {
+
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required field")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+
+    }
+
+}
